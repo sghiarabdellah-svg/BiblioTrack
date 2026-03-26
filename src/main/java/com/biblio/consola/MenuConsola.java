245bfd4034fdcfa54bf6bc684bc42ac3a1843c8d
sghiarabdellah-svg <sghiarabdellah@gmail.com>
2026-03-26 10:59:46 +0100

    Add functionality to borrow and return publications

--- src/main/java/com/biblio/consola/MenuConsola.java
+++ src/main/java/com/biblio/consola/MenuConsola.java
@@ -1,5 +1,17 @@
 package com.biblio.consola;
 
 public class MenuConsola {
-    
+    case 2:
+    System.out.print("Título: ");
+    String titulo = sc.nextLine();
+    System.out.print("Usuario: ");
+    String usuario = sc.nextLine();
+    catalogo.prestarPublicacion(titulo, usuario);
+    break;
+
+case 3:
+    System.out.print("Título: ");
+    titulo = sc.nextLine();
+    catalogo.devolverPublicacion(titulo);
+    break;
 }
